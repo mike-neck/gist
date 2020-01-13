94af237bfdd591a70c981a006d15d2ad81d5ebb7
mike_neck <jkrt3333@gmail.com>
2020-01-13 16:24:13 +0900

    gist-1 clone command - example of go-git.

--- clone_test.go
+++ clone_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -10,4 +15,37 @@ func TestPreferSSHFromBool(t *testing.T) {
 	assert.Equal(t, https, preferHTTPS)
 	preferSSH := PreferSSHFromBool(true)
 	assert.Equal(t, ssh, preferSSH)
+}
+
+// example of go-git
+func TestGitCloneExample(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "clone-example")
+	if err != nil {
+		assert.Fail(t, "failed to get temp dir: %s", err.Error())
+	}
+	fmt.Println(tempDir)
+	err = os.RemoveAll(tempDir)
+	if err != nil {
+		assert.Fail(t, "remove dir contents: %s", err.Error())
+		return
+	}
+	repository, err := git.PlainClone(tempDir, false, &git.CloneOptions{
+		URL: "git@gist.github.com:0674f0f942295225275c349abbe06675.git",
+	})
+	if err != nil {
+		assert.Fail(t, "failed to clone, %s", err.Error())
+		return
+	}
+	branches, err := repository.Branches()
+	if err != nil {
+		assert.Fail(t, "failed to get branches: %s", err.Error())
+		return
+	}
+	err = branches.ForEach(func(reference *plumbing.Reference) error {
+		fmt.Println("branch:", reference.Name(), reference.String(), reference.Hash())
+		return nil
+	})
+	if err != nil {
+		assert.Fail(t, "failed to show branches: %s", err.Error())
+	}
 }
